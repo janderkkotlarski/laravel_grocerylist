4b2d8e004580f165bd9804445b4abebd85bd3c99
janderkkotlarski <j.d.kotlarski@gmx.com>
2025-03-13 15:57:53 +0100

    getting local files to work is such a hassle, so just use something already online

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,9 @@
 <!-- invoke the layout meta-page -->
 <x-layout>
+    <!-- blade meta-linking -->
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
     <!-- the following is put into $slot for this page due to the layout invocation -->
     <h1>This is the Contact Page.</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 <!-- invoke the layout meta-page -->
 <x-layout>
+    <!-- blade meta-linking -->
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
     <!-- the following is put into $slot for this page due to the layout invocation -->
     <h1>This is the Home Page.</h1>
 </x-layout>
