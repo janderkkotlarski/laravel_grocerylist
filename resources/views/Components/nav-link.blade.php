a2c8f8e192e69b225171dd7920f1e7d12a225ac1
janderkkotlarski <j.d.kotlarski@gmx.com>
2025-03-14 15:30:35 +0100

    A lot to learn in one session

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,4 +1,11 @@
-<!-- Just a simple link with blade meta-coding -->
-<a {{ $attributes }}>{{ $slot }}</a>
+<!-- blade properties with 'active' as entry -->
+@props(['active' => false, 'type' => 'a'])
 
+
+
+<!-- Just a simple link with blade meta-coding -->
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" 
+aria-current="{{ $active ? 'page' : 'false'}}"
+{{ $attributes }}>{{ $slot }}</a>
+<!-- $attributes contains all the attributes of the selected nav link -->
 
