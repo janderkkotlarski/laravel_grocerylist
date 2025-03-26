a0b65615eb7043d024337e38dbd2f2cbeea73a3c
janderkkotlarski <j.d.kotlarski@gmx.com>
2025-03-26 14:47:29 +0100

    learned deleting rows can be done with the destroy command

--- app/Models/Celery.php
+++ app/Models/Celery.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Celery extends Model {
 	protected $table = 'celery_listings';
 
-	protected $fillable = ['title', 'celery'];
+	protected $fillable = ['title', 'celery'];	
 }
