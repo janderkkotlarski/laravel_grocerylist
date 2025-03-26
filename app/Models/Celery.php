e5d953bb96b0c71bd5b8e6ea4e20866a083383a5
janderkkotlarski <j.d.kotlarski@gmx.com>
2025-03-26 15:53:59 +0100

    learned to play around with factories

--- app/Models/Celery.php
+++ app/Models/Celery.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Celery extends Model {
+	use HasFactory;
+
 	protected $table = 'celery_listings';
 
 	protected $fillable = ['title', 'celery'];	
