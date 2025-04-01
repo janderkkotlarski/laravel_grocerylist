43b592c98ab24e5fd9db4371b54f26c40aa14f01
janderkkotlarski <j.d.kotlarski@gmx.com>
2025-04-01 14:55:11 +0200

    Changed some of the UI

--- resources/views/celeries.blade.php
+++ resources/views/celeries.blade.php
@@ -5,13 +5,16 @@
         Celery Opinions
     </x-slot:heading>
     <!-- Blade foreach loop -->
-     <ul>
+    <div class="space-y-4">
         @foreach ($celeries as $celery)
-            <li>
-                <a href="/celeries/{{ $celery['id'] }}" class="text-blue-500 hover:underline">
+            <a href="/celeries/{{ $celery['id'] }}" class="block px-4 py-6 border border-purple-200 rounded-lg">
+                <div class=" font-bold text-blue-500 text-sm">
+                    {{ $celery->employer->name }}
+                </div>
+                <div>
                     <strong>{{ $celery['title'] }}</strong>'s celery opinions: {{ $celery['celery'] }}
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
